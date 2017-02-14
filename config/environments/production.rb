496b0ec53fc058f66f1e585e74db27c9f2570ce3
clarksjf <sjf.clark@gmail.com>
2017-02-14 22:25:29 +0000

    email 17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ config.paperclip_defaults = {
   }
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => 'SENDGRID_USERNAME',
-    :password => 'SENDGRID_PASSWORD',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'debubble.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,

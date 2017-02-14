fe1b3d5b95346fa428349055d85364dbca7ba98f
clarksjf <sjf.clark@gmail.com>
2017-02-14 22:39:21 +0000

    email 18

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,12 @@ config.paperclip_defaults = {
     }
   }
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.default_url_options = { :host => 'http://myapp.herokuapp.com' }
+
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
